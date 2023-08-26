eb283e9f47ca5916de37c095d3990b4e79128602
tvgiriprasad <tvgiriprasad@gmail.com>
2023-08-26 06:05:37 +0000

    Branch to commit Loan Processing code

---
+++ loanprocess/src/main/java/com/hackathon/loanprocess/loanprocess/LoanprocessApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.loanprocess.loanprocess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanprocessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanprocessApplication.class, args);
+	}
+
+}
