91c0b419d99d65bcd24c2a865f8d92379e895573
tvgiriprasad <tvgiriprasad@gmail.com>
2023-08-26 05:30:30 +0000

    commit demo branch by team

---
+++ democode/src/main/java/com/demo/democode/DemocodeApplication.java
@@ -0,0 +1,13 @@
+package com.demo.democode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemocodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemocodeApplication.class, args);
+	}
+
+}
